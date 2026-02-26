1a3372c938231e96c2a0dfe7d79a988f41800111
Arpit Chakladar <arpitchakladar@proton.me>
2026-02-27 00:39:04 +0530

    Using ftxui to render

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
-#include <iostream>
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/component/loop.hpp>
 
 int main(int argc, char *argv[]) {
-	std::cout << "Hello, World!" << std::endl;
-	return EXIT_SUCCESS;
+	auto screen = ftxui::ScreenInteractive::Fullscreen();
+	auto button = ftxui::Button("Click me", [] {});
+
+	auto app = ftxui::Renderer(button, [&] {
+		bool focused = button->Focused();
+		return button->Render()
+			| ftxui::bgcolor(focused ? ftxui::Color::Blue : ftxui::Color::Black)
+			| ftxui::color(focused ? ftxui::Color::White : ftxui::Color::GrayLight)
+			| ftxui::border;
+	});
+
+	screen.Loop(app);
+
+	return 0;
 }
