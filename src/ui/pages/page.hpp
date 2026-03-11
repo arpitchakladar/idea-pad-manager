5257df533ced4319c9fab3f29419db6b0ad2daaf
Arpit Chakladar <arpitchakladar@proton.me>
2026-03-11 19:14:23 +0530

    Made some more changes

--- src/ui/pages/page.hpp
+++ src/ui/pages/page.hpp
@@ -3,29 +3,39 @@
 #include <ftxui/component/component_base.hpp>
 #include <ftxui/dom/canvas.hpp>
 #include <functional>
+#include <string>
+#include <tuple>
 
 #pragma once
 
 namespace idea_pad_manager::ui::pages {
+	using RowStatic = std::tuple<
+			std::string,
+			std::string
+		>;
+	using RowDynamic = std::tuple<
+			std::string,
+			std::function<std::string()>
+		>;
+	using RowCustom = std::tuple<
+			std::string,
+			std::function<std::string()>,
+			ftxui::Component
+		>;
+	
 	class Page {
 	public:
 		inline ftxui::Component component() { return _pageComponent; }
 		inline int canvasUpdatesPerSecond() { return _canvasUpdatesPerSecond; }
-
+	
 	protected:
 		Page() = default;
 		void CreatePage(
 			std::initializer_list<
-				std::pair<
-					std::string,
-					std::variant<
-						std::string,
-						std::function<std::string()>,
-						std::pair<
-							std::function<std::string()>,
-							ftxui::Component
-						>
-					>
+				std::variant<
+					RowStatic,
+					RowDynamic,
+					RowCustom
 				>
 			> rows,
 			std::string title,
