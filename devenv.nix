585c469f77a448ccf6adf2fe799e048da287f66d
Arpit Chakladar <arpitchakladar@proton.me>
2026-03-12 23:20:35 +0530

    Added project auto-formatting and pre-commit hook for auto formatting

--- devenv.nix
+++ devenv.nix
@@ -1,30 +1,41 @@
 { pkgs, ... }:
 
 {
-	env = {
-		CC = "${pkgs.clang}/bin/clang";
-		CXX = "${pkgs.clang}/bin/clang++";
-		CMAKE_EXPORT_COMPILE_COMMANDS = "1";
-		hardeningDisable = [ "fortify" ];
-	};
+  pre-commit.hooks = {
+    nixfmt.enable = true;
+    format-script = {
+      enable = true;
+      name = "format project";
+      entry = "./scripts/format.sh";
+      language = "script";
+      pass_filenames = false;
+    };
+  };
 
-	languages.cplusplus = {
-		enable = true;
-	};
+  env = {
+    CC = "${pkgs.clang}/bin/clang";
+    CXX = "${pkgs.clang}/bin/clang++";
+    CMAKE_EXPORT_COMPILE_COMMANDS = "1";
+    hardeningDisable = [ "fortify" ];
+  };
 
-	packages = with pkgs; [
-		cmake
-		clang-tools
-		ninja
-		clang
-		python3
-		include-what-you-use
-		clang-analyzer
-		llvmPackages.libcxxClang
-	];
+  languages.cplusplus = {
+    enable = true;
+  };
 
-	enterShell = ''
-		export CC=${pkgs.clang}/bin/clang
-		export CXX=${pkgs.clang}/bin/clang++
-	'';
+  packages = with pkgs; [
+    cmake
+    clang-tools
+    ninja
+    clang
+    python3
+    include-what-you-use
+    clang-analyzer
+    llvmPackages.libcxxClang
+  ];
+
+  enterShell = ''
+    export CC=${pkgs.clang}/bin/clang
+    export CXX=${pkgs.clang}/bin/clang++
+  '';
 }
