922f1d80a1fdd64b63be5b76956c60f9d07b8895
Arpit Chakladar <arpitchakladar@proton.me>
2026-03-13 01:49:16 +0530

    Separated the triangle drawing code for canvas into a separate utils class

--- src/ui/pages/PowerInformation.cpp
+++ src/ui/pages/PowerInformation.cpp
@@ -1,61 +1,13 @@
 #include "ui/pages/PowerInformation.hpp"
 
-#include "ui/pages/Page.hpp"
-
 #include <cmath>
 #include <ftxui/component/component.hpp>
 #include <ftxui/dom/canvas.hpp>
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/screen/color.hpp>
 
-namespace {
-
-auto DrawFilledTriangle(ftxui::Canvas &Canvas,
-  float X1,
-  float Y1,
-  float X2,
-  float Y2,
-  float X3,
-  float Y3,
-  const ftxui::Color &Color) -> void {
-  if (Y1 > Y2) {
-    std::swap(X1, X2);
-    std::swap(Y1, Y2);
-  }
-  if (Y1 > Y3) {
-    std::swap(X1, X3);
-    std::swap(Y1, Y3);
-  }
-  if (Y2 > Y3) {
-    std::swap(X2, X3);
-    std::swap(Y2, Y3);
-  }
-
-  if (Y1 == Y3)
-    return;
-
-  for (auto Y = static_cast<int>(Y1), EndY = static_cast<int>(Y3); Y <= EndY;
-    ++Y) {
-    const auto IsBottomHalf = static_cast<float>(Y) > Y2 || Y2 == Y1;
-    const auto SegmentHeight = IsBottomHalf ? (Y3 - Y2) : (Y2 - Y1);
-
-    const auto Alpha = (static_cast<float>(Y) - Y1) / (Y3 - Y1);
-    const auto Beta =
-      (static_cast<float>(Y) - (IsBottomHalf ? Y2 : Y1)) / SegmentHeight;
-
-    auto Ax = static_cast<int>(X1 + (X3 - X1) * Alpha);
-    auto Bx = static_cast<int>(
-      IsBottomHalf ? (X2 + (X3 - X2) * Beta) : (X1 + (X2 - X1) * Beta));
-
-    if (Ax > Bx)
-      std::swap(Ax, Bx);
-
-    for (auto X = Ax; X <= Bx; ++X) {
-      Canvas.DrawPoint(X, Y, true, Color);
-    }
-  }
-}
-} // namespace
+#include "ui/pages/Page.hpp"
+#include "ui/utils/CustomCanvas.hpp"
 
 namespace ipm::ui::pages {
 
@@ -99,7 +51,7 @@ PowerInformation::PowerInformation() {
     [&]() -> ftxui::Canvas {
       constexpr auto PI = std::numbers::pi_v<float>;
       constexpr auto BladeThickness = PI / 24.0F;
-      auto Canvas = ftxui::Canvas(100, 100);
+      auto Canvas = utils::CustomCanvas(100, 100);
       constexpr auto CenterX = 50;
       constexpr auto CenterY = 50;
       constexpr auto Radius = 40.0F;
@@ -109,9 +61,8 @@ PowerInformation::PowerInformation() {
           m_CurrentAngle + (static_cast<float>(I) * 2.0F * PI / 3.0F);
         const auto Theta = std::make_pair(
           BaseTheta - BladeThickness, BaseTheta + BladeThickness);
-        DrawFilledTriangle(Canvas,
-          50,
-          50,
+        Canvas.DrawFilledTriangle(CenterX,
+          CenterY,
           CenterX + Radius * std::cos(Theta.first),
           CenterY + Radius * std::sin(Theta.first),
           CenterX + Radius * std::cos(Theta.second),
